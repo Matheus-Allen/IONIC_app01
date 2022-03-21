88967aba2c2ffdd3d8f80463c863c613a0d3a90d
Matheus-Allen <94012940+Matheus-Allen@users.noreply.github.com>
2022-03-21 15:58:49 -0300

    20220321_02

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyB1sRyO7MHCVSdyRpgt2dKAGyAPByOURic",
-  authDomain: "my-profile-ace32.firebaseapp.com",
-  projectId: "my-profile-ace32",
-  storageBucket: "my-profile-ace32.appspot.com",
-  messagingSenderId: "254915137201",
-  appId: "1:254915137201:web:680ab95b41e2c7adfd1c90"
+    projectId: 'my-profile-ace32',
+    appId: '1:254915137201:web:680ab95b41e2c7adfd1c90',
+    storageBucket: 'my-profile-ace32.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyB1sRyO7MHCVSdyRpgt2dKAGyAPByOURic',
+    authDomain: 'my-profile-ace32.firebaseapp.com',
+    messagingSenderId: '254915137201',
   }
 };
